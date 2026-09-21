f175f20637b9250dbcabd5c8b4a978fe59069d5f
makyama17-arch <makyama17@gmail.com>
2026-09-21 23:53:40 +0300

    Update MainActivity.kt

--- app/src/main/java/com/makyama/audioplayer/MainActivity.kt
+++ app/src/main/java/com/makyama/audioplayer/MainActivity.kt
@@ -2,7 +2,6 @@ package com.makyama.audioplayer
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,14 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
-
-        val openMusicButton = findViewById<Button>(R.id.openMusicButton)
-
-        openMusicButton.setOnClickListener {
-            startActivity(
-                Intent(this, LibraryActivity::class.java)
+        // Open the music library immediately when the app starts.
+        startActivity(
+            Intent(
+                this,
+                LibraryActivity::class.java
             )
-        }
+        )
+
+        finish()
     }
 }
